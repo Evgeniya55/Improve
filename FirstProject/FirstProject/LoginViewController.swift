9ce210c1cddf48781a79afe4d7a137fb646f9af4
Евгения <senchurova.e@gmail.com>
2021-03-15 21:41:08 +0700

    initial commit

---
+++ FirstProject/FirstProject/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstProject
+//
+//  Created by Студент 3 on 15.03.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+
+        
+        titleLabel.text = "Hello"
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
