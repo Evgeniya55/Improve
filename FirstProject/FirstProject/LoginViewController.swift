a2ddb4256b5571f37e7c49b32bf14b518345e9a7
Евгения <senchurova.e@gmail.com>
2021-03-21 16:09:10 +0700

    There is a transition between TextFields when Return button is pressed (added switchFirstResponder func); Return button changes to "next" and "done" for login and password TextFields accordingly; KeyboardType for login is set to .emailAddress; Keyboard closes when there is a tap anywhere in a blank space; Added new custom class for UIButton which makes a button change it's background when highlighted; Slight fixes in constraints because TextField design's changed earlier (during classwork).

--- FirstProject/FirstProject/LoginViewController.swift
+++ FirstProject/FirstProject/LoginViewController.swift
@@ -9,13 +9,43 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var loginTextField: StyledTextField!
+    @IBOutlet weak var passwordTextField: StyledTextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registrationButton: UIButton!
+    
     override func viewDidLoad() {
 
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        loginTextField.delegate = self
+        passwordTextField.delegate = self
+        
+        loginTextField.keyboardType = .emailAddress
+        loginTextField.returnKeyType = .next
+        passwordTextField.returnKeyType = .done
+            
+        // hides the keyboard when user tappes outside of it
+        let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing(_:)))
+        view.addGestureRecognizer(tap)
+        
     }
-
-
+    
+    // func to switch between TextFields after Return's pressed
+    func switchFirstResponder(textField: UITextField) {
+        if textField == loginTextField {
+            passwordTextField.becomeFirstResponder()
+        } else if textField == passwordTextField {
+            loginTextField.becomeFirstResponder()
+        }
+    }
 }
 
+extension LoginViewController: UITextFieldDelegate {
+ 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        switchFirstResponder(textField: textField)
+        return true
+    }
+}
